4d37d3f3fbd9ea4b7bfaf494ba14e5d3743fd894
Travis <travisp@gmail.com>
2023-04-14 12:55:00 -0400

    Initial commit

---
+++ lib/rails_gpt_loader/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RailsGptLoader
+  VERSION = "0.1.0"
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "rails_gpt_loader"
+
+require "minitest/autorun"
